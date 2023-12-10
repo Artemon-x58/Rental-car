6ada00a74c8df07e724d7f8cd6ce2c9148072857
Artemon-x58 <artem1145819@gmail.com>
2023-12-10 13:26:39 +0100

    tet

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import { HashRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter basename="/Rental-car">
-        <App />
-      </HashRouter>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
