370fb84397a56f3460187332964a62f1e28c2b6b
Artemon-x58 <artem1145819@gmail.com>
2023-12-10 13:22:09 +0100

    test hushrouter

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/Rental-car">
+      <HashRouter basename="/Rental-car">
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
